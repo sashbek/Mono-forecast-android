7c62ac711dea31661e5a3bc4788b0f9186718d3d
jpegMushrum <artem070705@yandex.ru>
2026-02-18 17:19:43 +0300

    [feat] Added logs activity content

--- app/src/main/java/org/pakicek/monoforecast/LogsActivity.kt
+++ app/src/main/java/org/pakicek/monoforecast/LogsActivity.kt
@@ -12,6 +12,8 @@ class LogsActivity : AppCompatActivity() {
     private val binding
         get() = _binding ?: throw IllegalStateException("Binding must not be null")
 
+    private var isLogging = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityLogsBinding.inflate(layoutInflater)
@@ -22,6 +24,13 @@ class LogsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnStartLogging.setOnClickListener {
+            isLogging = !isLogging
+
+            binding.btnStartLogging.isSelected = isLogging
+            binding.btnStartLogging.text = if (isLogging) "Stop Logging" else "Start Logging"
         }
 
         binding.btnBack.setOnClickListener {
